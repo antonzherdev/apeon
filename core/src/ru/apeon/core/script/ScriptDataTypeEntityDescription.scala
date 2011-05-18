67d2d7189a4decc502cad9f8d8d6f3b64d2936c7
Anton Zherdev <zherdev@apeon.ru>
2011-05-18 17:50:30 +0400

    Опциональный поиск по идентификатору и сущность Employee в комтехе.

--- core/src/ru/apeon/core/script/ScriptDataTypeEntityDescription.scala
+++ core/src/ru/apeon/core/script/ScriptDataTypeEntityDescription.scala
@@ -7,7 +7,7 @@ import eql.DataSourceExpressionDataSource
 
 case class ScriptDataTypeEntityDescription(description : Description) extends ScriptDataType {
   private val obj : Option[ObjectBase] = description.pack.model.objOption(description.fullName)
-  override val declarations = Seq(applyId, applyEql, insert, findEql) ++ obj.map(_.declarations).getOrElse(Seq())
+  override val declarations = Seq(applyId, applyEql, insert, findEql, findId) ++ obj.map(_.declarations).getOrElse(Seq())
 
 
   def insert = new Declaration {
@@ -18,16 +18,26 @@ case class ScriptDataTypeEntityDescription(description : Description) extends Sc
     def correspond(env: Environment, parameters: Option[Seq[Par]]) = parameters.isEmpty
   }
 
-  def applyId = new Declaration {
-    def value(env: Environment, parameters: Option[Seq[ParVal]], dataSource: Option[Expression]) =
+  def applyId = new FindId {
+    override def value(env: Environment, parameters: Option[Seq[ParVal]], dataSource: Option[Expression]) =
+      super.value(env, parameters, dataSource).asInstanceOf[Option[Entity]].getOrElse{
+          throw ScriptException(env, "Entity %s not found for id %d".format(description, parameters.get.head.value))
+      }
+    override def name = "apply"
+    override def dataType(env: Environment, parameters : Option[Seq[Par]]) = ScriptDataTypeEntityByDescription(description)
+  }
+
+  def findId = new FindId
+
+  class FindId extends Declaration {
+    def value(env: Environment, parameters: Option[Seq[ParVal]], dataSource: Option[Expression]) : Any =
       parameters.get.head.value match {
-        case id : Int => env.em.get(new SqlEntityId(env.dataSource(dataSource), description, id)).getOrElse(
-          throw ScriptException(env, "Entity %s not found for id %d".format(description, id))
-        )
+        case id : Int => env.em.get(new SqlEntityId(env.dataSource(dataSource), description, id))
         case _ => throw ScriptException(env, "Not integer")
       }
-    def name = "apply"
-    def dataType(env: Environment, parameters : Option[Seq[Par]]) = ScriptDataTypeEntityByDescription(description)
+    def name = "find"
+    def dataType(env: Environment, parameters : Option[Seq[Par]]) : ScriptDataType =
+      ScriptDataTypeOption(ScriptDataTypeEntityByDescription(description))
     def correspond(env: Environment, parameters: Option[Seq[Par]]) = parameters match {
       case Some(Seq(par)) => par.expression.dataType(env) == ScriptDataTypeInteger()
       case _ => false
@@ -46,7 +56,7 @@ case class ScriptDataTypeEntityDescription(description : Description) extends Sc
   def findEql = new FindEql
 
   class FindEql extends Declaration {
-    def value(env: Environment, parameters: Option[Seq[ParVal]], dataSource: Option[Expression])  = {
+    def value(env: Environment, parameters: Option[Seq[ParVal]], dataSource: Option[Expression]) : Any  = {
       val where = parameters.get.head.value.asInstanceOf[eql.Expression]
       val select = eql.Select(eql.FromEntity(description, None,
         DataSourceExpressionDataSource(env.dataSource(dataSource))), where = Some(where))
